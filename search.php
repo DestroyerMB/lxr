0b216cb4dab50738ee4d55c98d9b38101ee6ae23
DestroyerMB <vechernij.zvon@gmail.com>
2017-03-29 05:33:56 -0700

    Add files via upload

--- search.php
+++ search.php
@@ -3,23 +3,26 @@ header("Content-Type: application/json; charset=UTF-8");
 $callback = $_GET["callback"];
 $data = json_decode($_GET["data"], false);
 
-/*$conn = new mysqli("myServer", "myUser", "myPassword", "Northwind");
-$result = $conn->query("SELECT name FROM ".$obj->$table." LIMIT ".$obj->$limit);
-$outp = array();
-$outp = $result->fetch_all(MYSQLI_ASSOC);
-
-echo "myFunc(".json_encode($outp).")";*/
-
-/*$myJSON = '{ "name":"'.$data->name.'", "age":30, "city":"New York" }';
-
-echo "(".$myJSON.");";*/
-
 $rs = array();
-$row = "{ \"name\":\"".$data->name."\", \"age\":30, \"city\":\"New York\" }";
-array_push($rs,$row);
-$row2 = '{ "name":"yyy", "age":37, "city":"Chicago" }';
+if($data->id == 1)
+{
+  $row1 = '{ "name":"Room", "price":650, "city":"Trier" }';
+  $row2 = '{ "name":"Another room", "price":700, "city":"Luxembourg" }';
+}
+elseif($data->id == 2)
+{
+  $row1 = '{ "name":"Apartment", "price":1200, "city":"Chicago" }';
+  $row2 = '{ "name":"Nice apartment", "price":1100, "city":"Boston" }';
+}
+elseif($data->id == 3)
+{
+  $row1 = '{ "name":"House", "price":1300, "city":"Wormeldange" }';
+  $row2 = '{ "name":"House", "city":"Sandweiler" }';
+}
+
+array_push($rs,$row1);
 array_push($rs,$row2);
 
-echo "myFunc(".json_encode($rs).")";
+echo $callback."(".json_encode($rs).")";
 
 ?>
