160a232383499047ba3013bf191d8a62b42739e0
DestroyerMB <vechernij.zvon@gmail.com>
2017-06-12 12:24:01 +0200

    test viewport change

--- map_test.php
+++ map_test.php
@@ -13,12 +13,13 @@
   <div id="map" style="width: 600px; height: 500px;"></div>
 
   <script type="text/javascript">
+
   function showMapLocation(lont,long) { //49.609660, 6.404947
     var locations = [
       [1, 'Apartments in Wormeldange', lont, long , false, '2 rooms<br>1 floor']
     ];
 
-    var map = new google.maps.Map(document.getElementById('map'), {
+    map = new google.maps.Map(document.getElementById('map'), {
       zoom: 15,
       center: new google.maps.LatLng(lont, long),
       mapTypeId: google.maps.MapTypeId.ROADMAP
@@ -34,7 +35,10 @@
         map: map
       });
 
-	  google.maps.event.addListener(marker, 'mouseover', (function(marker, i) {
+      google.maps.event.addListener(map, 'idle', function() {
+        showViewport();
+      });
+    google.maps.event.addListener(marker, 'mouseover', (function(marker, i) {
         return function() {
 			infowindow.setContent('<h3>'+locations[i][1]+'</h3>'+locations[i][5]);
 			infowindow.open(map, this);
@@ -57,6 +61,17 @@
       })(marker, i));
     }
   }
+
+  function showViewport() {
+    var lat0 = map.getBounds().getNorthEast().lat();
+    var lng0 = map.getBounds().getNorthEast().lng();
+    var lat1 = map.getBounds().getSouthWest().lat();
+    var lng1 = map.getBounds().getSouthWest().lng();
+    ;;console.log(lat0+', '+lng0+'  '+lat1+', '+lng1);
+  }
+
+
+
   </script>
 </body>
 </html>
