0dbc57e180e4bfb57bf8f27a97c5d5795322c95b
DestroyerMB <vechernij.zvon@gmail.com>
2017-07-07 11:39:53 +0200

    Add files via upload

---
+++ resize.php
@@ -0,0 +1,20 @@
+<html>
+<head>
+
+</head>
+<body>
+    <input type="button" value="Resize Image" id="resize" />
+    <!--<input type="range" id="size" min="128" max="1024">-->
+    <br/>
+    <div>
+        <h1>Resized Image</h1>
+        <img id="outputImage" />
+    </div>
+    <div>
+        <h1>Original Image</h1>
+        <img id="inputImage" src="example.jpg" width="1024" height="768" />
+    </div>
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js"></script>
+    <script src="resize.js"></script>
+</body>
+</html>
